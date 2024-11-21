f79d1a9df95efe262fb9d638515b6eb352a63cd2
anujsb <141896390+anujsb@users.noreply.github.com>
2024-11-21 19:19:30 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='text-6xl'>
+      hi
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            logo name
+        </div>
+    </div>
+  )
+}
+
+export default Header
